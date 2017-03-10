86d556a1cc0199c6ae5eda3827f33b8e1e474c0e
SHERRY <swasieko@gmail.com>
2017-03-10 00:11:02 +0000

    Add validations

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -3,4 +3,8 @@ class Restaurant < ActiveRecord::Base
   mount_uploader :image, ImageUploader
   
   has_many :reviews
+  
+  validates :name, :address, :phone, :website, :image, presence: true
+  validates :phone, format: { with: /\A\(\d{3}\) \d{3}-\d{4}\z/,
+    message: "must be in the format (123) 345-7890" }
 end
